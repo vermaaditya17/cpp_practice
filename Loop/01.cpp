9967d7c2a3da650fb9848b8eef7b70206863d88e
vermaaditya17 <145982553+vermaaditya17@users.noreply.github.com>
2026-03-27 15:34:04 +0530

    loop added

---
+++ Loop/01.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+// For loop 
+int main(){
+    for(int i = 0; i<5; i++){// int initialise value; condition ; pre increment
+        cout << i << "\n"; // \n is used for break the line 
+    }
+    return 0;
+}
+/* output
+1
+2
+3
+4 */
