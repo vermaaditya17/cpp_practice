6866065f40e88932b6e976da5c766f45f9579c44
vermaaditya17 <145982553+vermaaditya17@users.noreply.github.com>
2026-03-27 15:46:17 +0530

    sum of number

---
+++ Loop/03.cpp
@@ -0,0 +1,15 @@
+// sum of number 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int sum = 0; // should make variable 
+    for(int i=0 ; i<=30; i++){
+
+        sum = sum + i; // above variable declare every time with loop condition 
+    }
+    cout << "sum is :" << sum ;
+return 0;
+
+}
+// Output is --> sum is :465
